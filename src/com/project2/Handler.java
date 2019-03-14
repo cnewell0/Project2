9a62ebadd4f226fc62561ac98e78a52aa335b75a
cadenewell <cnewell@zagmail.gonzaga.edu>
2019-03-14 14:36:50 +0100

    Main finished, handler started

---
+++ src/com/project2/Handler.java
@@ -0,0 +1,11 @@
+package com.project2;
+
+import javax.swing.text.Document;
+
+public class Handler {
+
+
+  public static void handleChannelTag(Document document) {
+
+  }
+}
--- src/com/project2/Main.java
+++ src/com/project2/Main.java
@@ -1,16 +1,23 @@
 package com.project2;
 
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.*;
 import java.io.*;
+import java.util.logging.Handler;
 
 public class Main {
-  public static void main(String[] argv) {
+  public static void main(String[] argv) throws ParserConfigurationException, IOException, SAXException {
+
+    File xmlFile = new File("hamlet.xml");
 
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     DocumentBuilder builder = factory.newDocumentBuilder();
-    System.out.println("wasssup C");
+
+    Document document = builder.parse(xmlFile);
+
+    document.getDocumentElement().normalize();
 
 
   }
